761971806c5e0e46e4dc65cd18af7ad5d8f86cc9
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2024-11-18 12:43:24 -0300

    Add exibição das perguntas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Welcome from "./components/Welcome"
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
 
 import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
 
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   );
 }
